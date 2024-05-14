7c4136dd00ccff459db6e61d212ed0eb87e13803
Abdelaaziz Amx <itmasterdesigne@gmail.com>
2024-05-14 11:59:24 +0100

    Update 5-app.py

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -50,7 +50,7 @@ def get_locale():
 @app.route("/", methods=['GET'])
 def index():
     """index rotue"""
-    return render_template("6-index.html")
+    return render_template("5-index.html")
 
 
 if __name__ == "__main__":
